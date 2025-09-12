7469db9d771873cb7c50484c8bed0ff4ec4b042d
akashpanda-01 <1999akashpanda@gmail.com>
2025-09-12 23:45:33 +0530

    react practice project

---
+++ Components/TabButton.jsx
@@ -0,0 +1,7 @@
+export default function TabButton ({children, onSelect, isSelect}){
+    return(
+        <li>
+            <button className={isSelect ? "active" : undefined} onClick={onSelect} >{children}</button>
+        </li>
+    )
+};
