c591f6056141b5c0e18d823a8ff5a7dae3c03af7
Akash <1999akashpanda@gmail.com>
2025-11-10 19:33:48 +0530

    New-Project

--- Practice-Project-2/App.jsx
+++ Practice-Project-2/App.jsx
@@ -1,11 +1,49 @@
 import React from "react";
+import { useState } from "react";
+import "./App.css";
 
 const App = () => {
-    return (
-        <>
-        <h1>Project-2</h1>
-        </>
-    )
+  const [color, setColor] = useState("olive");
+
+  return (
+    <>
+      <div className="main" style={{ backgroundColor: color }}>
+        <div className="btn-list">
+          <button
+            style={{ backgroundColor: "red", color: "white" }}
+            onClick={() => setColor("red")}
+          >
+            RED
+          </button>
+          <button
+            style={{ backgroundColor: "blue", color: "white" }}
+            onClick={() => setColor("blue")}
+          >
+            BLUE
+          </button>
+          <button
+            style={{ backgroundColor: "green", color: "white" }}
+            onClick={() => setColor("green")}
+          >
+            GREEN
+          </button>
+          <button
+          className=""
+            style={{ backgroundColor: "yellow", color: "white" }}
+            onClick={() => setColor("yellow")}
+          >
+            YELLOW
+          </button>
+          <button
+            style={{ backgroundColor: "black", color: "white" }}
+            onClick={() => setColor("black")}
+          >
+            BLACK
+          </button>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default App;
