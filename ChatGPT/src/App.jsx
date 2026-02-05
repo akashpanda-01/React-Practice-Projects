86f6ecf71322baf2b93e285d476a3b64bbbdf2ab
Akash <1999akashpanda@gmail.com>
2026-02-05 16:31:13 +0530

    ChatGPT clone

--- ChatGPT/src/App.jsx
+++ ChatGPT/src/App.jsx
@@ -1,17 +1,27 @@
 // import { useState } from 'react'
-
-import './App.css'
+import logo from "./assets/ChatGPT-Logo.jpg";
+import plusLogo from "./assets/add-30.png";
+import "./App.css";
 
 function App() {
-
   return (
     <>
-      <div>
-        <h1 className=''>Hello World</h1>
+      <div className="app">
+        <div className="sideBar">
+          <div className="upperSide">
+            <div className="flex">
+              <img className="logo" src={logo} alt="" />
+              <span className="">ChatGPT</span>
+            </div>
+            <button className="midBtn"><img className="addBtn" src={plusLogo} />New Chat</button>
+            <div className="upperSideBottom"></div>
+          </div>
+          <div className="lowerSide"></div>
+        </div>
+        <div className="main"></div>
       </div>
-      
     </>
-  )
+  );
 }
 
-export default App
+export default App;
